9b0f6f688e010bb79efe57c442d70df8d3577b02
GitLab Runner <gitlab-runner@gitlab-runner.int>
2024-05-22 09:58:24 +0200

    initial commit

---
+++ sampleproject/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace sampleproject;
+
+public class MyClass
+{
+    public static string SomeTestableMethod()
+    {
+	var culture = CultureInfo.GetCultures(CultureTypes.NeutralCultures).Single(c=>c.TwoLetterISOLanguageName == "en");
+        DateTime datetime = new(1978, 06, 02);
+	return datetime.ToString(culture);
+    }
+}
---
+++ sampleprojecttests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using sampleproject;
+
+namespace sampleprojecttests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+	Assert.Equal(MyClass.SomeTestableMethod(), "06/02/1978 00:00:00 AM");
+    }
+}
